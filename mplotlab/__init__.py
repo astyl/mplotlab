060990009055df3cea012b871596e775a842b185
André ASTYL <andreastyl@hotmail.com>
2016-02-08 02:08:16 +0100

    update date & version

--- mplotlab/__init__.py
+++ mplotlab/__init__.py
@@ -1,28 +1,14 @@
-# -*-coding:Utf-8 -*
-#
-# Name:      wxmplot
-# Version:   0.9.14
-# Purpose:   Provide user-configurable 2D plotting module, using wxPython
-#            and matplotlib.
-# Author:    Andre ASTYL
-# Copyright: Andre ASTYL
-# Licence:   MIT license
-# Updated:   21/10/2015
-#-----------------------------------------------------------------------------
-
-""" description here
-"""
-__version__  = '0.1'
-__date__     = '21/10/2015'
+__version__  = '0.2'
+__date__     = '08/02/2016'
 
 import sys, wx
 try:
     if not hasattr(sys, 'frozen'):
         import wxversion
-        wxversion.ensureMinimal('2.8')
+        wxversion.ensureMinimal('2.9')
 except ImportError:
     pass
-except: # probably a useless exception from ensureMinimal() about wx already being Imported.
+except:
     pass
 
 
